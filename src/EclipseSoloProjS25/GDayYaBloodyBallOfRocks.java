4d1e495173af6a3f87ea5fd332632cea7bbfa47f
BoBucklin <xbobucklinx@gmail.com>
2025-07-10 23:29:04 -0400

    INISHUL KOMIT

---
+++ src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
@@ -0,0 +1,8 @@
+package EclipseSoloProjS25;
+public class GDayYaBloodyBallOfRocks {
+
+	public static void main(String[] args) {
+		System.out.println("DIS 'ERE BLOKE IZ 2 BLOODY STCHEWPID 2 UNDAHSTAND DIS PROJEK FING.");
+	}
+
+}
