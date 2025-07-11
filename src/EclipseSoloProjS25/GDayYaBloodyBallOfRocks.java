0ffbbe4a5ebc31aba132a6e1fe810579564f6864
BoBucklin <xbobucklinx@gmail.com>
2025-07-11 00:11:21 -0400

    Programmer 2 added code

--- src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
+++ src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
@@ -3,6 +3,7 @@ public class GDayYaBloodyBallOfRocks {
 
 	public static void main(String[] args) {
 		System.out.println("DIS 'ERE BLOKE IZ 2 BLOODY STCHEWPID 2 UNDAHSTAND DIS PROJEK FING.");
+		System.out.println("Programmer 2 did this.");
 	}
 
 }
