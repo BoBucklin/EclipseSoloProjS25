114f73d225eff9107dc0f7a32791abe3f1e7e9f3
BoBucklin <xbobucklinx@gmail.com>
2025-07-14 11:36:05 -0400

    More alt. workspace code

--- src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
+++ src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
@@ -3,6 +3,7 @@ public class GDayYaBloodyBallOfRocks {
 
 	public static void main(String[] args) {
 		System.out.println("More stuff. More stuff.");
+		System.out.println("Programmer One code.");
 		System.out.println("Programmer 2 did this.");
 	}
 
