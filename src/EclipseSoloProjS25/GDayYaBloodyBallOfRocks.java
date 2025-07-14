d388271c6534de949beddadbf892df81977ea199
BoBucklin <xbobucklinx@gmail.com>
2025-07-14 10:42:56 -0400

    Merge branch 'main' of https://github.com/BoBucklin/EclipseSoloProjS25.git into main

--- src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
+++ src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
@@ -2,8 +2,8 @@ package EclipseSoloProjS25;
 public class GDayYaBloodyBallOfRocks {
 
 	public static void main(String[] args) {
-		System.out.println("DIS 'ERE BLOKE IZ 2 BLOODY STCHEWPID 2 UNDAHSTAND DIS PROJEK FING.");
-		System.out.println("I'M BOUTA DIE. DIS TOOK 2 MUCH NOGGIN POWER.");
+		System.out.println("Stuff.");
+		System.out.println("This is takin too much brain power.");
 	}
 
 }
