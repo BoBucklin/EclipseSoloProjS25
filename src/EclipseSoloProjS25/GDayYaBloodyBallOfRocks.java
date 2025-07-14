f2f07a56116430bf5ae81cc68730a5f5feac6580
BoBucklin <xbobucklinx@gmail.com>
2025-07-14 11:19:58 -0400

    Programmer 2 did stuff

--- src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
+++ src/EclipseSoloProjS25/GDayYaBloodyBallOfRocks.java
@@ -2,7 +2,7 @@ package EclipseSoloProjS25;
 public class GDayYaBloodyBallOfRocks {
 
 	public static void main(String[] args) {
-		System.out.println("More stuff.");
+		System.out.println("More stuff. More stuff.");
 		System.out.println("Programmer 2 did this.");
 	}
 
